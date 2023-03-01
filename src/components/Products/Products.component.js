cd645c2c45183dca9d70aa07ade783d56844c33c
nishok19 <nishokrt1905@gmail.com>
2023-03-01 17:49:49 +0530

    Sort by price functionality is done.

--- src/components/Products/Products.component.js
+++ src/components/Products/Products.component.js
@@ -1,18 +1,45 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sortProductsStore } from "src/slices/productSlice";
 import ProductCard from "./ProductCard.component";
 
 const Products = ({ categoryId }) => {
+  const [sort, setSort] = useState("");
   const products = useSelector((state) => state.products.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(sortProductsStore(sort));
+  }, [sort]);
 
   return (
-    <div className="flex flex-wrap mx-auto ">
-      {!categoryId
-        ? products.map((prod) => <ProductCard item={prod} key={prod?._id} />)
-        : products.map((prod) => {
-            return prod?.collectionId == categoryId ? (
-              <ProductCard item={prod} key={prod?._id} />
-            ) : null;
-          })}
+    <div className="flex flex-wrap flex-col mx-auto">
+      <div className="flex item-center justify-end my-4">
+        <p className="text-center my-auto">Sort: </p>
+        <select
+          className="select select-bordered w-full max-w-xs"
+          onChange={(e) => {
+            setSort(e.target.value);
+          }}
+          value={sort}
+        >
+          <option disabled selected>
+            Sort
+          </option>
+          <option value="asc">Sort By Price: Low to High</option>
+          <option value="desc">Sort By Price: High to Low</option>
+        </select>
+      </div>
+
+      <div className="flex flex-wrap mx-auto">
+        {!categoryId
+          ? products.map((prod) => <ProductCard item={prod} key={prod?._id} />)
+          : products.map((prod) => {
+              return prod?.collectionId == categoryId ? (
+                <ProductCard item={prod} key={prod?._id} />
+              ) : null;
+            })}
+      </div>
     </div>
   );
 };
--- src/slices/productSlice.js
+++ src/slices/productSlice.js
@@ -16,9 +16,20 @@ export const productSlice = createSlice({
       state.products = [];
     },
 
-    //
+    // add searched products to the slice
     addSearchedProductsStore: (state, { payload }) => {
       state.searchedProducts = payload;
+    },
+
+    // sorting the products
+    sortProductsStore: (state, { payload }) => {
+      payload == null && null;
+      const sortedProducts = [...state.products].sort((a, b) => {
+        if (payload === "asc") return a.price - b.price;
+        if (payload === "desc") return b.price - a.price;
+      });
+      console.log("ooooooooo", sortedProducts);
+      state.products = sortedProducts;
     },
   },
 });
@@ -27,6 +38,7 @@ export const {
   addProductsStore,
   resetProductsStore,
   addSearchedProductsStore,
+  sortProductsStore,
 } = productSlice.actions;
 
 export default productSlice.reducer;
