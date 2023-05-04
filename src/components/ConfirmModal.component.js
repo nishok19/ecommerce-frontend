c7e775eef3de70dc2c85e0959255196feafd5a56
nishok19 <nishokrt1905@gmail.com>
2023-05-05 02:04:14 +0530

    Added UI for deleting a category

---
+++ src/components/ConfirmModal.component.js
@@ -0,0 +1,42 @@
+import { Description } from "@mui/icons-material";
+
+const ConfirmModal = ({ btnTitle, title, description, isConfirmDialog }) => {
+  return (
+    <>
+      {/* The button to open modal */}
+      <label htmlFor="my-modal-confirm-dialog" className="btn">
+        {btnTitle}
+      </label>
+
+      {/* Put this part before </body> tag */}
+      <input
+        type="checkbox"
+        id="my-modal-confirm-dialog"
+        className="modal-toggle"
+      />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">{title}</h3>
+          <p className="py-4">{description}</p>
+          <div className="modal-action">
+            <button
+              htmlFor="my-modal-confirm-dialog"
+              className="btn"
+              onClick={() => isConfirmDialog(true)}
+            >
+              Confirm
+            </button>
+            <button
+              htmlFor="my-modal-confirm-dialog"
+              className="btn"
+              onClick={() => isConfirmDialog(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default ConfirmModal;
--- src/components/Profile/AdminCategory.component.js
+++ src/components/Profile/AdminCategory.component.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToastStore } from "src/slices/toastSlice";
 import { addNewCollection } from "src/utils/collection.utils";
+import ConfirmModal from "../ConfirmModal.component";
 
 const Category = () => {
   const [collectionName, setCollectionName] = useState("");
+  const [delCollectionName, setDelCollectionName] = useState("");
+  const categoryStore = useSelector((state) => state.category.category);
 
   const dispatch = useDispatch();
 
@@ -35,9 +38,14 @@ const Category = () => {
     console.log("Success...created the category");
   };
 
+  const deleteCollection = async (isConfirm) => {
+    if (isConfirm) console.log("wwwoooooo");
+    else console.log("weeeeeeeeeeeee");
+  };
+
   return (
     <div className="mt-7">
-      <h3 className="text-3xl">Manage Category</h3>
+      <h3 className="text-3xl font-semibold">Manage Category</h3>
       <div className="form-control">
         <label className="label mt-4">
           <span className="label-text text-xl">Add new category</span>
@@ -53,6 +61,34 @@ const Category = () => {
           <button onClick={addCollection} className="btn">
             Add
           </button>
+        </div>
+      </div>
+      {/*  */}
+      <div className="mt-10">
+        <div className="text-3xl font-normal">Delete a Category of Product</div>
+        <div className="form-control">
+          <div className="input-group">
+            <select
+              className="select select-bordered"
+              value={delCollectionName}
+              onChange={(e) => setDelCollectionName(e.target.value)}
+            >
+              <option disabled selected>
+                Pick category
+              </option>
+              {categoryStore.map((cat) => (
+                <option key={cat?._id}>{cat?.name}</option>
+              ))}
+            </select>
+            <label className="">
+              <ConfirmModal
+                btnTitle={"Delete"}
+                title={"Confirm"}
+                description={"Are you sure you want to delete this category"}
+                isConfirmDialog={deleteCollection}
+              />
+            </label>
+          </div>
         </div>
       </div>
     </div>
